2ec5c0c766dcb5c9643f2b8df189b2d7c5692fbe
ABoiledCarny <aboiledcarny@cbailey.tech>
2019-10-29 14:50:55 -0700

    Complete test requirements as setup.py extras

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-from setuptools import setup
 from pathlib import Path
+from setuptools import setup
 
 ROOT = Path(__file__).parent
 PKG_NAME = 'objectmapper'
@@ -20,14 +20,34 @@ classifiers = [
     'Topic :: Software Development :: Testing :: Unit',
 ]
 
+docs_require = [
+    'sphinx==2.2.0',
+]
+
+tests_require = docs_require + [
+    'pylint==2.4.3',
+    'pytest==5.2.2',
+    'pytest-cov==2.8.1',
+    'pytype==2019.10.17',
+    'tox==3.14.0',
+]
+
+publish_require = tests_require + [
+    'twine==2.0.0',
+    'wheel==0.33.6',
+]
+
+extras_require = {
+    'docs': docs_require,
+    'tests': tests_require,
+    'publish': publish_require,
+}
+
 setup(
     author=about['__author__'],
     author_email=about['__author_email__'],
     description=about['__description__'],
-    extras_require={
-        'docs': 'sphinx==2.2.0',
-        'dev': 'twine==2.0.0'
-    },
+    extras_require=extras_require,
     include_package_data=True,
     license=about['__license__'],
     long_description=Path(ROOT, 'README.rst').open('r').read(),
